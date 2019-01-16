c2a909778b30377aeab09e923fabbcc3cdad606b
malfatle2 <42657476+malfatle2@users.noreply.github.com>
2019-01-16 11:48:59 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,33 @@
+/**whats up
+
+ * Author:Mohammed alfatle
+
+ * Date: 2019/01/16
+
+ *
+
+ * A simple hello world program in C
+
+ *
+
+ */
+
+#include<stdlib.h>
+
+#include<stdio.h>
+
+
+
+int main(int argc, char **argv) {
+
+
+
+
+
+  printf("hello, my name is mohammed alfatle/n");
+
+
+
+  return 0;
+
+}
